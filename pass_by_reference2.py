0b3c32f6695662f3a225a604bbbdf56079266b75
Patrick Lai <pcllai@gmail.com>
2023-10-19 19:07:23 +0800

    Pass by reference

--- pass_by_reference2.py
+++ pass_by_reference2.py
@@ -1,23 +1,36 @@
-#pass by value for function
+#1) pass by value for function
 def fn1(n):
     n = n + 1
-
+    print(n)
+    
 a = 1
 fn1(a)
+fn1(a)
 
-print (a)
 
+#2) pass by reference for function
+def fn2(n):
+    n[0] = n[0] + 1
+    print(n[0])
+    
+x = [1]    
+fn2(x)
+fn2(x)
 
-#pass by reference for function
+#3) pass by reference for class
 class Person:
     def __init__(self, age):
         self.age = age
-    
-    def addAge(self):
-        self.age = self.age + 1
         
-p = Person(3)
+    def addAge(self, number):
+        self.age = self.age + number
+        
+p = Person(30)
 print(p.age)
-p.addAge()
+p.addAge(2)
+print(p.age)
+q = p
+q.addAge(3)
+print(p.age)
+print(q.age)
 
-print(p.age)
