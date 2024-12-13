752cfc16a97a8337ebaa94bc65db0a29f1563b13
Patrick Lai <pcllai@gmail.com>
2024-12-13 22:52:55 +0800

    Able to decrypt files in directory and skip file if no password

--- remove_pdf_password.py
+++ remove_pdf_password.py
@@ -4,40 +4,58 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Process some arguments.')
-parser.add_argument('--file', type=str, help='File Path', required=True)
+parser.add_argument('--file', type=str, help='File Path', required=False)
+parser.add_argument('--dir', type=str, help='Directory', required=False)
 parser.add_argument('--pw', type=str, help='Password', required=True)
-
-# Parse the arguments
 args = parser.parse_args()
 
-# Print the argument
-print(f"The argument passed is: {args.file} {args.pw}")
-
-# Specify the input and output PDF file paths
-input_pdf_path = args.file
-password = args.pw
-
-try:
-    # Open the protected PDF with the password
-    with pikepdf.open(input_pdf_path, password=password) as pdf:
-        # Save a new PDF without the password
-        pdf.save(input_pdf_path + ".tmp")
-    
+def has_password(file_path):
     try:
-    # Rename the file
-        os.remove(input_pdf_path)
-        os.rename(input_pdf_path + ".tmp", input_pdf_path)
-        print(f"Password removed and saved as '{input_pdf_path}'")
-    
+        with pikepdf.open(file_path) as pdf:
+            return False
+
+    except pikepdf.PasswordError:
+        return True
+    except Exception as e:
+        raise
+
+def remove_file_password(file_path, password):
+    try:
+        if has_password(file_path):
+            # Open the protected PDF with the password
+            with pikepdf.open(file_path, password=password) as pdf:
+                # Save a new PDF without the password
+                pdf.save(file_path + ".tmp")
+            
+            # Close the tmp file and Rename the file to original name
+            os.remove(file_path)
+            os.rename(file_path + ".tmp", file_path)
+            print(f"Password removed for file: '{file_path}'")
+        else:
+            print(f"The file {file_path} doesn't have password")
+        
     except FileNotFoundError:
-        print(f"Error: The file '{input_pdf_path}' does not exist.")
+        print(f"Error: The file '{file_path}' does not exist.")
     except PermissionError:
-        print(f"Error: You do not have permission to rename '{input_pdf_path}'.")
+        print(f"Error: You do not have permission to rename '{file_path}'.")
+    except pikepdf._core.PasswordError:
+        print("Failed to open the PDF: incorrect password.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
-except pikepdf.PasswordError:
-    print("Failed to open the PDF: incorrect password.")
-except Exception as e:
-    print(f"An error occurred: {e}")
 
+def remove_dir_password(directory, password):
+    try:
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                remove_file_password(os.path.join(dirpath, filename), password)
+                #print(os.path.join(dirpath, filename))
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if args.dir != "":
+    remove_dir_password(args.dir, args.pw)
+else:
+    remove_file_password(args.file, args.pw)
